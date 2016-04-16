ec7da2f9d2d6621b7a96c7466c4f27423854705a
Darcks OMega <Darcks OMega@DarcksOMega-PC>
2016-04-16 18:44:31 -0500

    Imprimir lista

---
+++ ABCTrees/src/ABC/Node.java
@@ -0,0 +1,21 @@
+package ABC;
+
+/**
+ *
+ * @author Darcks OMega
+ */
+public class Node {
+    
+    protected int data;
+    protected Node left;
+    protected Node right;
+    
+    public Node(int d){
+        
+        data = d;
+        left = null;
+        right = null;
+        
+    }
+    
+}
